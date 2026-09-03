0ff593f5c48bb2e0d3ab2d6996229ca1de7e3b63
N_Ivan <ivan.ganeev007@gmail.com>
2026-09-03 15:35:31 +0700

    написал первую часть лабораторки

---
+++ ConsoleApp1/ConsoleApp1/LearningObjectives.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class LearningObjectives
+    {
+        public LearningObjectives(DateOnly dateOnly, TimeOnly timeOnly, string teacherName)
+        {   
+            Date = dateOnly;
+            Time = timeOnly;
+            TeacherName = teacherName;
+            
+        }
+        public DateOnly Date {  get; set; }
+        public TimeOnly Time { get; set; }
+        public string TeacherName { get; set; }
+        public string GetAll()
+        {
+            return $"дата: {Date.ToString("dd.MM.yyyy")} время: {Time.ToString("HH:mm")}, преподаватель: \"{TeacherName}\"";
+        }
+    }
+    
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleApp1;
+
+List<LearningObjectives> list = new List<LearningObjectives>
+{
+    new LearningObjectives(new DateOnly(2026, 6, 6), new TimeOnly(10, 15), "Иванов Иван Иванович"),
+    new LearningObjectives(new DateOnly(2026, 6, 6), new TimeOnly(12, 0), "Иванов Иван Иванович"),
+    new LearningObjectives(new DateOnly(2026, 6, 6), new TimeOnly(14, 10), "Иванов Иван Иванович"),
+    new LearningObjectives(new DateOnly(2026, 6, 6), new TimeOnly(16, 55), "Иванов Иван Иванович"),
+};
+for (int i = 0; i < list.Count; i++)
+{
+    Console.WriteLine(list[i].GetAll());
+}
