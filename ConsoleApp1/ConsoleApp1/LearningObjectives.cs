20864fa4d3460058229ae529e886e1dea50817e1
N_Ivan <ivan.ganeev007@gmail.com>
2026-09-10 14:52:36 +0700

    добавлена функция поиска максимальной суммы 3 идущих подряд чисел из списка

--- ConsoleApp1/ConsoleApp1/LearningObjectives.cs
+++ ConsoleApp1/ConsoleApp1/LearningObjectives.cs
@@ -17,6 +17,7 @@ namespace ConsoleApp1
         public DateOnly Date {  get; set; }
         public TimeOnly Time { get; set; }
         public string TeacherName { get; set; }
+        
 
         public override string ToString()
         {
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,12 +1,45 @@
 ﻿using ConsoleApp1;
 
-Console.WriteLine("напишите в формате дд.мм.гггг чч:мм \"фио\"");
+
 string fileText = File.ReadAllText("D:\\IS-SFU\\ConsoleApp1\\data.txt");
+List<string> lines = new List<string>();
+List<LearningObjectives> learningObjectives = new List<LearningObjectives>();
+
+Console.WriteLine("напишите в формате дд.мм.гггг чч:мм \"фио\"");
+foreach (string line in fileText.Split('\n'))
+{
+    lines.Add(line);
+}
 Console.WriteLine(fileText);
-//string str = Console.ReadLine();
-//DateOnly date = DateOnly.Parse(str[0..10]);
-//TimeOnly time = TimeOnly.Parse(str[11..16]);
-//int Index1 = str.IndexOf("\"")+1;
-//string Name = str[Index1..^1];
-//LearningObjectives learningObjectives = new LearningObjectives(date, time, Name);
-//Console.WriteLine(learningObjectives.ToString());
+string str = Console.ReadLine();
+foreach (string line in lines)
+{
+    LearningObjectives learningObjective = CreateObject(line);
+    learningObjectives.Add(learningObjective);
+    Console.WriteLine(learningObjective.ToString());
+}
+
+int f(List<int> list)
+{
+    int max = 0;
+    for (int i = 2; i < list.Count; i++)
+    {
+        int three = list[i - 2] + list[i - 1] + list[i];
+        if (three > max)
+        {
+            max = three;
+        }
+    }
+    return max;   
+}
+
+
+LearningObjectives CreateObject(string str)
+{
+    DateOnly date = DateOnly.Parse(str[0..10]);
+    TimeOnly time = TimeOnly.Parse(str[11..16]);
+    int Index1 = str.IndexOf("\"") + 1;
+    string Name = str[Index1..^1];
+    LearningObjectives learningObjective = new LearningObjectives(date, time, Name);
+    return learningObjective;
+}
