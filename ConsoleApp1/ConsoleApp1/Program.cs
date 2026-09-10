3990b78bae0f5ff645187a47b45319685c6b35c9
N_Ivan <ivan.ganeev007@gmail.com>
2026-09-10 14:46:35 +0700

    добавил функцию преоразования строки в объект

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,10 +1,12 @@
 ﻿using ConsoleApp1;
 
 Console.WriteLine("напишите в формате дд.мм.гггг чч:мм \"фио\"");
-string str = Console.ReadLine();
-DateOnly date = DateOnly.Parse(str[0..10]);
-TimeOnly time = TimeOnly.Parse(str[11..16]);
-int Index1 = str.IndexOf("\"")+1;
-string Name = str[Index1..^1];
-LearningObjectives learningObjectives = new LearningObjectives(date, time, Name);
-Console.WriteLine(learningObjectives.ToString());
+string fileText = File.ReadAllText("D:\\IS-SFU\\ConsoleApp1\\data.txt");
+Console.WriteLine(fileText);
+//string str = Console.ReadLine();
+//DateOnly date = DateOnly.Parse(str[0..10]);
+//TimeOnly time = TimeOnly.Parse(str[11..16]);
+//int Index1 = str.IndexOf("\"")+1;
+//string Name = str[Index1..^1];
+//LearningObjectives learningObjectives = new LearningObjectives(date, time, Name);
+//Console.WriteLine(learningObjectives.ToString());
